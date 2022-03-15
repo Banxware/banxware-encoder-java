09cb84417a8e2c00554e426e07b0f88326bee60c
Barney Dennis <barney.dennis@banxware.com>
2022-03-15 13:12:49 +0000

    Updated compression to use gzip not brotli. Bumped version.

--- src/main/java/com/banxware/Compressor.java
+++ src/main/java/com/banxware/Compressor.java
@@ -1,29 +1,25 @@
 package com.banxware;
 
-import com.nixxcode.jvmbrotli.enc.BrotliOutputStream;
-import com.nixxcode.jvmbrotli.enc.Encoder;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.zip.GZIPOutputStream;
 
 class Compressor {
     public static byte[] compress(String in) throws IOException {
         InputStream inFile = new ByteArrayInputStream(in.getBytes());
         ByteArrayOutputStream outFile = new ByteArrayOutputStream();
 
-        Encoder.Parameters params = new Encoder.Parameters().setQuality(4).setWindow(16);
-
-        BrotliOutputStream brotliOutputStream = new BrotliOutputStream(outFile, params);
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(outFile);
 
         int read = inFile.read();
         while (read > -1) { // -1 means EOF
-            brotliOutputStream.write(read);
+            gzipOutputStream.write(read);
             read = inFile.read();
         }
 
-        brotliOutputStream.close();
+        gzipOutputStream.close();
         inFile.close();
 
         return outFile.toByteArray();
